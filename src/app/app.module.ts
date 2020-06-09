230381f8ebb59eb7cca51b365a416ea815cfe21c
ayotycoon <kaysehventeen@gmail.com>
2020-06-09 16:26:24 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,11 @@ import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
 import { DatatableModule } from "inn-datatable";
 
 @NgModule({
   imports: [BrowserModule, HttpClientModule, FormsModule, DatatableModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
